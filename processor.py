17b813465978683fa39a4d1f9e67877d246841e2
sirlilpanda <sirlilpanda@gmail.com>
2023-01-06 09:52:39 +1300

    processor has now been made in to a main.py of sorts

---
+++ processor.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+import re
+from document_tree import Document
+
+def load_file(filename) -> str:
+    with open(filename, "r+") as txt:
+        return txt.read()
+
+def main():
+    file = load_file("example.yi")    
+    doc = Document("test")
+    doc.parse_file(file)
+    print(doc)
+
+if __name__ == "__main__":
+    main()
