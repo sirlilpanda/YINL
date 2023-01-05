08f2bcc40b573a325eade6e35ea62d2203f0713d
JHay0112 <jordanhay0112@gmail.com>
2023-01-06 11:12:24 +1300

    add dataclasses for Footer and Header

--- document_tree.py
+++ document_tree.py
@@ -1,19 +1,53 @@
+"""
+    Defines nodes of the YINL document tree.
+"""
+
+from typing import Callable
+from dataclasses import dataclass
+from datetime import datetime
 
 class Node:
-    def __init__(self, data : str, parent : 'Node') -> None:
+    def __init__(self, data : str, parent : 'Node' = None) -> None:
         self.parent : 'Node' = parent
-        self.childern : list['Node'] = list()
+        self.children : list['Node'] = list()
         self.data : str = data
     
     def __repr__(self) -> str:
         return f"(id : {id(self)}\ndata: {self.data}\nparent:{id(self.parent)}\n\tamount:{len(self.childern)})\n"
 
     def add_child(self, node : 'Node'):
-        self.childern.append(node)
+        node.parent = self
+        self.children.append(node)
+
+@dataclass
+class Header:
+    """
+        Stores document header information as per YINL spec.
+        This is meant to be a direct reflection of the document header in the YINL file.
+    """
+    document: 'Document'
+    title: str = None
+    authors: list[str] = []
+    date: datetime = None
+    institute: str = None
+    citation_style: str = None      
+
+@dataclass
+class Footer:
+    """
+        Stores document footer information as per YINL spec.
+        This is meant to be a direct reflection of the document footer in the YINL file.
+    """
+    document: 'Document'
+    citations: dict[str, str] = dict()
+    macros: dict[str, Callable] = dict()
+    shorthands: dict[str, str] = dict()
 
 class Document:
     def __init__(self, name: str) -> None:
+        self.header = Header(self)
         self.root = Node(name, None)
+        self.footer = Footer(self)
     
     def parse_file(self, file:str):
         raw_file : list[str] = file.split("\n")
