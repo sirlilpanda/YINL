5f12e36b92e53a3edd34f60d1e0c93f36fd98e97
JHay0112 <jordanhay0112@gmail.com>
2023-01-06 14:37:35 +1300

    add section pattern matching

--- pattern_matching.py
+++ pattern_matching.py
@@ -5,10 +5,18 @@
 import re
 
 MACRO_PATTERN = r"[a-zA-Z\-_]{1,50}: {0,1}\([a-zA-Z0-9\,. -/]*\)"
+SECTION_START_PATTERN = r"section [A-Za-z0-9?!. ]*:(?! {0,1}\(.*)"
 
-def find_macros(text: str) -> list[tuple[int, int]]:
+def find_macros(text: str) -> list[re.Match]:
     """
         Finds likely macros within a piece of text,
         returns a list of regex match objects
     """
-    return re.findall(MACRO_PATTERN, text, re.MULTILINE) 
+    return list(re.finditer(MACRO_PATTERN, text))
+
+def find_sections(text: str) -> list[re.Match]:
+    """
+        Finds likely sections within a piece of text,
+        returns a list of regex match objects
+    """
+    return list(re.finditer(SECTION_START_PATTERN, text))
--- tests/test_pattern_matching.py
+++ tests/test_pattern_matching.py
@@ -2,14 +2,15 @@
     Pytest tests for testing pattern matching module.
 """
 
-from ..pattern_matching import find_macros
+from ..pattern_matching import find_macros, find_sections
 
 with open("tests/test.yi", "r") as f:
     EXAMPLE_TEXT = f.read()
     EXAMPLE_NUM_MACROS = 6
+    EXAMPLE_NUM_SECTIONS = 2
 
 def test_find_macros():
+    assert len(find_macros(EXAMPLE_TEXT)) == EXAMPLE_NUM_MACROS
 
-    found_macros = find_macros(EXAMPLE_TEXT)
-    print(found_macros)
-    assert len(found_macros) == EXAMPLE_NUM_MACROS
+def test_find_sections():
+    assert len(find_sections(EXAMPLE_TEXT)) == EXAMPLE_NUM_SECTIONS
