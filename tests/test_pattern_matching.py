eb7bf88a01239d528abf38b759b7e8924ae8e925
JHay0112 <jordanhay0112@gmail.com>
2023-01-06 14:06:40 +1300

    github script for building and running tests

--- tests/test_pattern_matching.py
+++ tests/test_pattern_matching.py
@@ -5,10 +5,11 @@
 from ..pattern_matching import find_macros
 
 with open("tests/test.yi", "r") as f:
-    TEST_TEXT = f.read()
+    EXAMPLE_TEXT = f.read()
+    EXAMPLE_NUM_MACROS = 6
 
 def test_find_macros():
 
-    found_macros = find_macros(TEST_TEXT)
+    found_macros = find_macros(EXAMPLE_TEXT)
     print(found_macros)
-    assert len(found_macros) == 6
+    assert len(found_macros) == EXAMPLE_NUM_MACROS
