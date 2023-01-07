b24bd4b4737a3d4f8a25dea516f6bb3c963859c3
JHay0112 <jordanhay0112@gmail.com>
2023-01-07 17:12:04 +1300

    get section info from section regex

--- pattern_matching.py
+++ pattern_matching.py
@@ -4,19 +4,65 @@
 
 import re
 
-MACRO_PATTERN = r"[a-zA-Z\-_]{1,50}: {0,1}\([a-zA-Z0-9\,. -/]*\)"
-SECTION_START_PATTERN = r"section [A-Za-z0-9?!. ]*:(?! {0,1}\(.*)"
+from math import gcd
+from functools import reduce
 
-def find_macros(text: str) -> list[re.Match]:
+MACRO_PATTERN = r"[a-zA-Z\-_]{1,50}: {0,1}\([a-zA-Z0-9\,. -/]*\)"
+SECTION_START_PATTERN = r" *section .*: {0,1}(?! {0,1}\(.*)"
+
+def get_indent(line: str) -> int:
     """
-        Finds likely macros within a piece of text,
+        Determines the size of the indent of a line.
+    """
+    return len(line) - len(line.lstrip())
+
+def locate_macros(text: str) -> list[re.Match]:
+    """
+        Finds likely macro posiitons within a piece of text,
         returns a list of regex match objects
     """
     return list(re.finditer(MACRO_PATTERN, text))
 
-def find_sections(text: str) -> list[re.Match]:
+def locate_sections(text: str) -> list[re.Match]:
     """
-        Finds likely sections within a piece of text,
+        Finds likely section positions within a piece of text,
         returns a list of regex match objects
     """
     return list(re.finditer(SECTION_START_PATTERN, text))
+
+def get_sections(text: str) -> list[tuple[str, int, str]]:
+    """
+        Gets sections and their contents from a piece of text.
+        Note that section content may include subsections, macros, and shorthands.
+        Returns a list of tuples of the form (section_name, section_indent, section_content)
+    """
+    section_locations = locate_sections(text)
+    indents = [get_indent(text[:section.end()].splitlines()[-1]) for section in section_locations]
+    
+    indent_size = reduce(gcd, indents)
+    if indent_size == 0:
+        indent_size = 4 # default to 4 spaces if no indent is found
+
+    sections = []
+    for section, indent in zip(section_locations, indents):
+
+        section_name = section.group().strip().split(":")[0][7:].strip()
+        section_indent = indent // indent_size
+
+        # get the content of the section
+        section_content = ""
+        for line in text[section.end():].splitlines()[1:]:
+            if get_indent(line) // indent_size > section_indent:
+                # include lines that are indented more than the section
+                section_content += line.strip() + "\n"
+            elif len(line) == 0:
+                # inclue empty lines
+                section_content += "\n"
+            else:
+                # break if the line is less indented than the section and is not empty
+                # this indicates the section has ended :)
+                break
+        sections.append((section_name, section_indent, section_content))
+
+    return sections
+    
--- tests/test_pattern_matching.py
+++ tests/test_pattern_matching.py
@@ -2,15 +2,33 @@
     Pytest tests for testing pattern matching module.
 """
 
-from ..pattern_matching import find_macros, find_sections
+from ..pattern_matching import get_indent, locate_macros, locate_sections, get_sections
 
 with open("tests/test.yi", "r") as f:
     EXAMPLE_TEXT = f.read()
     EXAMPLE_NUM_MACROS = 6
     EXAMPLE_NUM_SECTIONS = 2
 
-def test_find_macros():
-    assert len(find_macros(EXAMPLE_TEXT)) == EXAMPLE_NUM_MACROS
+def test_get_indent():
+    assert get_indent("    a") == 4
+    assert get_indent("a") == 0
+    assert get_indent(" a") == 1
+    assert get_indent("") == 0
+    assert get_indent("a    ") == 0
 
-def test_find_sections():
-    assert len(find_sections(EXAMPLE_TEXT)) == EXAMPLE_NUM_SECTIONS
+def test_locate_macros():
+    assert len(locate_macros(EXAMPLE_TEXT)) == EXAMPLE_NUM_MACROS
+
+def test_locate_sections():
+    assert len(locate_sections(EXAMPLE_TEXT)) == EXAMPLE_NUM_SECTIONS
+
+def test_get_sections():
+    sections = get_sections(EXAMPLE_TEXT)
+
+    assert sections[0][0] == "Introduction" # title
+    assert sections[0][1] == 0              # indent level
+    
+    assert sections[1][0] == "Why YINL?" # title
+    assert sections[1][1] == 1           # indent level
+
+    assert len(sections) == EXAMPLE_NUM_SECTIONS
