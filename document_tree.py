08697e957cff28355eb00a2e5ce3d39a202b6d76
Logan Henderson <lhe68@uclive.ac.nz>
2023-01-09 15:34:49 +1300

    added footer parses and updated header to accpet multi line values

--- document_tree.py
+++ document_tree.py
@@ -83,43 +83,71 @@ class Document:
         """
             Parses the document header from an open file.
         """
+        tab_space_amount = 4 #the amount of spaces in a tab
         lines = text.splitlines()
         header_start = lines.index("header:")
-        print(lines)
-        header_end = lines[header_start:].index("")                #plus one here so header is not included in header lines
-        header_lines = [line.strip() for line in lines[header_start+1:header_end] if line != ""]
-
+        header_end = lines[header_start:].index("") #TODO needs to be fixed so a new line doesnt need to exist to end the header
+        
+        #plus one here so header is not included in header lines
+        header_lines = [line[tab_space_amount:] for line in lines[header_start+1:header_end] if line != ""]
         i = 0
         while i < len(header_lines):
             line = header_lines[i]
-
             if line.endswith(":") and not line.endswith("\\:"):
-                # key with value on next line
+                # key with value on next lines
                 key = line[:-1]
-                value = header_lines[i+1]
-                i += 1 # skip value line
-            elif ":" in line and "\:" not in line:
-                # key with value on same line
-                key, value = line.split(":")
-                value = value.strip()
-            else:
-                # not a key!
-                # so skip
-                i += 1
-                continue
+                value_index = i+1 #the start of the values
+                tabbed = True
+                # will go through the header untill the next no indented index is found
+                while tabbed and value_index < len(header_lines):
+                    if header_lines[value_index].startswith(" "*tab_space_amount):
+                        value_index += 1
+                    else:
+                        tabbed = False
+                # joins the results together with commas
+                value = ",".join([val.strip() for val in header_lines[i+1:value_index]])
+                i = value_index-1
             
-            if key == "author": #as there maybe more then one author setting
+            if key in ("authors", "author"): #as there maybe more then one author setting
                 self.header.__setattr__(key, [author.strip() for author in value.split(",")])
-            elif self.header.__getattribute__(key) == None:
+            elif self.header.__getattribute__(key) == None: # this checks for if the attribute exists
                 self.header.__setattr__(key, value)
             i += 1
 
     def parse_footer(self, text: str):
         """
-            Parses the document header from an open file.
+            Parses the document footer from an open file.
         """
-        footer_start = text.splitlines().index("footer:")
-        # TODO: implement
+        #TODO parse macros correctly
+        tab_space_amount = 4 #make global
+        lines = text.splitlines()
+        footer_start = lines.index("footer:")
+        footer_end = len(lines) #end of the file                
+        #plus one here so footer is not included in footer lines
+        footer_lines = [line[tab_space_amount:] for line in lines[footer_start+1:footer_end] if line != ""]
+        i = 0
+        while i < len(footer_lines):
+            line = footer_lines[i]
+            if line.endswith(":") and not line.endswith("\\:"):
+                # key with value on next lines
+                key = line[:-1]
+                value_index = i+1 # the start of the values
+                tabbed = True
+                # will go through the header untill the next no indented index is found
+                while tabbed and value_index < len(footer_lines):
+                    print(footer_lines[value_index])
+                    if footer_lines[value_index].startswith(" "*tab_space_amount):
+                        value_index += 1
+                    else:
+                        tabbed = False
+                # cleans up the values
+                value = [val.strip() for val in footer_lines[i+1:value_index]]
+                i = value_index-1
+
+            # makes sure the attribute exist in the footer object
+            if self.footer.__getattribute__(key) == None:
+                self.footer.__setattr__(key, value)
+            i+=1
 
     def parse_body(self, text: str):
         """
@@ -145,7 +173,7 @@ class Document:
 
         self.parse_header(text[header.start():header.end()])
         self.parse_footer(text[footer.start():footer.end()])
-        self.parse_body(text[header.end():footer.start()])
+        # self.parse_body(text[header.end():footer.start()])
 
     def parse_file(self, file: str):
         """
